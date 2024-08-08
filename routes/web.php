30c475a85ff8a2a243c0d3112bb729a63c1974a9
Harsh Kedia <hkedia@hey.com>
2024-08-08 09:46:07 +0530

    use a controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ExampleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return '<h1>Home Page</h1><a href="/about">View the about Page</a>';
-});
+Route::get('/', [ExampleController::class, "homepage"]);
 
-Route::get("/about", function () {
-    return '<h1>About Page</h1><a href="/">Back to Home</a>';
-});
+Route::get("/about", [ExampleController::class, "aboutpage"]);
