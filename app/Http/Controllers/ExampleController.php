ac92e31445c7838ec2dde6b9168a6cc33e58c60d
Harsh Kedia <hkedia@hey.com>
2024-08-08 10:42:56 +0530

    using blade components

--- app/Http/Controllers/ExampleController.php
+++ app/Http/Controllers/ExampleController.php
@@ -7,13 +7,10 @@ use Illuminate\Http\Request;
 class ExampleController extends Controller
 {
     public function homepage() {
-        $ourName = 'Harsh';
-        $animals = ['Meowalot', 'Barksalot', 'Purrsloud'];
-
-        return view('homepage', ['allAnimals' => $animals, 'name' => $ourName, 'catname' => 'Meowalot']);
+        return view('homepage');
     }
 
     public function aboutpage() {
-        return '<h1>About Page!!</h1><a href="/">Back to Home</a>';
+        return view('single-post');
     }
 }
