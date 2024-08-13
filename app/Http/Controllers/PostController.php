7b8133c51fa7f297460af2aca9a3a4f5110f889d
Harsh Kedia <hkedia@hey.com>
2024-08-13 10:49:24 +0530

    markdown support

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -27,6 +28,7 @@ class PostController extends Controller
     }
 
     public function viewSinglePost(Post $post) {
+        $post['body'] = strip_tags(Str::markdown($post->body), '<p><ul><ol><li><strong><em><h3><br>');
         return view('single-post', ['post' => $post]);
     }
 }
